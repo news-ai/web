233f1a6c6071a78688cefa4b35dc536df03759e2
Abhi Agarwal <me@abhiagarwal.com>
2016-11-25 17:48:24 -0500

    refresh token

--- google/google.go
+++ google/google.go
@@ -1,16 +1,24 @@
 package google
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/news-ai/tabulae/models"
 
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
+
+	"golang.org/x/net/context"
 )
+
+const BASEURL = "https://www.googleapis.com/"
 
 type User struct {
 	ID            string `json:"id"`
@@ -22,6 +30,19 @@ type User struct {
 	Picture       string `json:"picture"`
 	Locale        string `json:"locale"`
 	Hd            string `json:"hd"`
+}
+
+type RefreshTokenRequest struct {
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+	RefreshToken string `json:"refresh_token"`
+	GrantType    string `json:"grant_type"`
+}
+
+type RefreshTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int    `json:"expires_in"`
+	TokenType   string `json:"token_type"`
 }
 
 // Check if access token is valid
@@ -55,5 +76,54 @@ func ValidateAccessToken(r *http.Request, user models.User) error {
 
 func RefreshAccessToken(r *http.Request, user models.User) error {
 	c := appengine.NewContext(r)
+
+	contextWithTimeout, _ := context.WithTimeout(c, time.Second*15)
+	client := urlfetch.Client(contextWithTimeout)
+
+	if user.RefreshToken == "" {
+		return errors.New("User does not have a refresh token")
+	}
+
+	refreshtokenRequest := RefreshTokenRequest{}
+	refreshtokenRequest.ClientID = os.Getenv("GOOGLEAUTHKEY")
+	refreshtokenRequest.ClientSecret = os.Getenv("GOOGLEAUTHSECRET")
+	refreshtokenRequest.RefreshToken = user.RefreshToken
+	refreshtokenRequest.GrantType = "refresh_token"
+
+	messageJson, err := json.Marshal(refreshtokenRequest)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return err
+	}
+
+	messageQuery := bytes.NewReader(messageJson)
+
+	URL := BASEURL + "oauth2/v4/token"
+	req, _ := http.NewRequest("POST", URL, messageQuery)
+
+	response, err := client.Do(req)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return err
+	}
+
+	defer response.Body.Close()
+
+	toReturn := RefreshTokenResponse{}
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return err
+	}
+
+	err = json.Unmarshal(contents, &toReturn)
+	if err != nil {
+		log.Errorf(c, "%v", err)
+		return err
+	}
+
+	log.Infof(c, "%v", response.Body)
+	log.Infof(c, "%v", contents)
+
 	return nil
 }

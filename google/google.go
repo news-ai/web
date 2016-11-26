a2013cabcf593ce22dcc00d26607fd31594d61fe
Abhi Agarwal <me@abhiagarwal.com>
2016-11-25 19:21:07 -0500

    refresh user token works

--- google/google.go
+++ google/google.go
@@ -3,7 +3,6 @@ package google
 import (
 	"encoding/json"
 	"errors"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
@@ -41,9 +40,9 @@ type RefreshTokenRequest struct {
 
 type RefreshTokenResponse struct {
 	AccessToken string `json:"access_token"`
+	IDToken     string `json:"id_token"`
 	ExpiresIn   int    `json:"expires_in"`
 	TokenType   string `json:"token_type"`
-	IdToken     string `json:"id_token"`
 }
 
 // Check if access token is valid
@@ -75,18 +74,17 @@ func ValidateAccessToken(r *http.Request, user models.User) error {
 	return errors.New("Access token expired")
 }
 
-func RefreshAccessToken(r *http.Request, user models.User) error {
+func RefreshAccessToken(r *http.Request, user models.User) (models.User, error) {
 	c := appengine.NewContext(r)
 
 	contextWithTimeout, _ := context.WithTimeout(c, time.Second*15)
 	client := urlfetch.Client(contextWithTimeout)
 
 	if user.RefreshToken == "" {
-		return errors.New("User does not have a refresh token")
+		return user, errors.New("User does not have a refresh token")
 	}
 
 	URL := BASEURL + "oauth2/v4/token"
-	req, _ := http.NewRequest("POST", URL, nil)
 
 	form := url.Values{}
 	form.Add("client_id", os.Getenv("GOOGLEAUTHKEY"))
@@ -94,42 +92,29 @@ func RefreshAccessToken(r *http.Request, user models.User) error {
 	form.Add("refresh_token", user.RefreshToken)
 	form.Add("grant_type", "refresh_token")
 
-	req.PostForm = form
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	response, err := client.Do(req)
+	response, err := client.PostForm(URL, form)
 	if err != nil {
 		log.Errorf(c, "%v", err)
-		return err
+		return user, err
 	}
 
-	defer response.Body.Close()
-
-	toReturn := RefreshTokenResponse{}
-	contents, err := ioutil.ReadAll(response.Body)
+	// Decode JSON from Google
+	decoder := json.NewDecoder(response.Body)
+	var refreshtoken RefreshTokenResponse
+	err = decoder.Decode(&refreshtoken)
 	if err != nil {
 		log.Errorf(c, "%v", err)
-		return err
+		return user, err
 	}
 
-	err = json.Unmarshal(contents, &toReturn)
-	if err != nil {
-		log.Errorf(c, "%v", err)
-		return err
-	}
-
-	log.Infof(c, "%v", response.Body)
-	log.Infof(c, "%v", contents)
-
-	timeToAdd := time.Duration(time.Duration(toReturn.ExpiresIn) * time.Second)
-
-	if toReturn.AccessToken != "" {
-		user.AccessToken = toReturn.AccessToken
+	if refreshtoken.AccessToken != "" {
+		timeToAdd := time.Duration(time.Duration(refreshtoken.ExpiresIn) * time.Second)
+		user.AccessToken = refreshtoken.AccessToken
 		user.GoogleExpiresIn = time.Now().Add(timeToAdd)
-		user.TokenType = toReturn.TokenType
+		user.TokenType = refreshtoken.TokenType
 		user.Save(c)
-		return nil
+		return user, nil
 	}
 
-	return errors.New("Access token not present")
+	return user, errors.New("Access token not present")
 }

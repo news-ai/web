73c2913204eed07eec4072cdce695286eb82c360
Abhi Agarwal <me@abhiagarwal.com>
2016-11-27 21:48:45 -0500

    encrypting pw

---
+++ encrypt/decrypt.go
@@ -0,0 +1,40 @@
+package encrypt
+
+/*
+ - http://codereview.stackexchange.com/questions/125846/encrypting-strings-in-golang
+*/
+
+func decryptAES(key, data []byte) ([]byte, error) {
+	// split the input up in to the IV seed and then the actual encrypted data.
+	iv := data[:aes.BlockSize]
+	data = data[aes.BlockSize:]
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	stream := cipher.NewCFBDecrypter(block, iv)
+
+	stream.XORKeyStream(data, data)
+	return data, nil
+}
+
+// Takes two strings, cryptoText and keyString.
+// cryptoText is the text to be decrypted and the keyString is the key to use for the decryption.
+// The function will output the resulting plain text string with an error variable.
+func decryptString(cryptoText string, keyString string) (plainTextString string, err error) {
+	encrypted, err := base64.URLEncoding.DecodeString(cryptoText)
+	if err != nil {
+		return "", err
+	}
+	if len(encrypted) < aes.BlockSize {
+		return "", fmt.Errorf("cipherText too short. It decodes to %v bytes but the minimum length is 16", len(encrypted))
+	}
+
+	decrypted, err := decryptAES(hashTo32Bytes(keyString), encrypted)
+	if err != nil {
+		return "", err
+	}
+
+	return string(decrypted), nil
+}
---
+++ encrypt/encrypt.go
@@ -0,0 +1,47 @@
+package encrypt
+
+/*
+ - http://codereview.stackexchange.com/questions/125846/encrypting-strings-in-golang
+*/
+
+// hashTo32Bytes will compute a cryptographically useful hash of the input string.
+func hashTo32Bytes(input string) []byte {
+	data := sha256.Sum256([]byte(input))
+	return data[0:]
+}
+
+func encryptAES(key, data []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	// create two 'windows' in to the output slice.
+	output := make([]byte, aes.BlockSize+len(data))
+	iv := output[:aes.BlockSize]
+	encrypted := output[aes.BlockSize:]
+
+	// populate the IV slice with random data.
+	if _, err = io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+
+	// note that encrypted is still a window in to the output slice
+	stream.XORKeyStream(encrypted, data)
+	return output, nil
+}
+
+// Takes two string, plainText and keyString.
+// plainText is the text that needs to be encrypted by keyString.
+// The function will output the resulting crypto text and an error variable.
+func encryptString(plainText string, keyString string) (cipherTextString string, err error) {
+	key := hashTo32Bytes(keyString)
+	encrypted, err := encryptAES(key, []byte(plainText))
+	if err != nil {
+		return "", err
+	}
+
+	return base64.URLEncoding.EncodeToString(encrypted), nil
+}

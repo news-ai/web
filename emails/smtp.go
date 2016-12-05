fd7b5055dc488a516a6db22c5d0269a03a459c9a
Abhi Agarwal <me@abhiagarwal.com>
2016-12-04 19:16:23 -0500

    generating email body

--- emails/smtp.go
+++ emails/smtp.go
@@ -2,10 +2,115 @@ package emails
 
 import (
 	"crypto/tls"
+	"encoding/base64"
 	"errors"
 	"net"
+	"net/http"
 	"net/smtp"
+	"strings"
+
+	"github.com/news-ai/tabulae/attach"
+	"github.com/news-ai/tabulae/models"
 )
+
+func GenerateEmail(from string, to string, subject string, body string, email models.Email) (string, error) {
+	CC := ""
+	BCC := ""
+
+	if len(email.CC) > 0 {
+		CC = "Cc: " + strings.Join(email.CC, ",") + "\r\n"
+	}
+
+	if len(email.BCC) > 0 {
+		BCC = "Bcc: " + strings.Join(email.BCC, ",") + "\r\n"
+	}
+
+	temp := []byte("From: " + from + "\r\n" +
+		CC +
+		BCC +
+		"reply-to: " + from + "\r\n" +
+		"Content-type: text/html;charset=iso-8859-1\r\n" +
+		"MIME-Version: 1.0\r\n" +
+		"To:  " + to + "\r\n" +
+		"Subject: " + subject + "\r\n" +
+		"\r\n" + body)
+
+	raw := base64.StdEncoding.EncodeToString(temp)
+	raw = strings.Replace(raw, "/", "_", -1)
+	raw = strings.Replace(raw, "+", "-", -1)
+	raw = strings.Replace(raw, "=", "", -1)
+
+	return raw, nil
+}
+
+func GenerateEmailWithAttachments(r *http.Request, from string, to string, subject string, body string, email models.Email, files []models.File) (string, error) {
+	nl := "\r\n" // newline
+	boundary := "__newsai_tabulae__"
+
+	CC := ""
+	BCC := ""
+
+	if len(email.CC) > 0 {
+		CC = "Cc: " + strings.Join(email.CC, ",") + nl
+	}
+
+	if len(email.BCC) > 0 {
+		BCC = "Bcc: " + strings.Join(email.BCC, ",") + nl
+	}
+
+	temp := []byte("MIME-Version: 1.0" + nl +
+		"To:  " + to + nl +
+		CC +
+		BCC +
+		"From: " + from + nl +
+		"reply-to: " + from + nl +
+		"Subject: " + subject + nl +
+
+		"Content-Type: multipart/mixed; boundary=\"" + boundary + "\"" + nl + nl +
+
+		// Boundary one is email itself
+		"--" + boundary + nl +
+
+		"Content-Type: text/html; charset=UTF-8" + nl +
+		"MIME-Version: 1.0" + nl +
+		"Content-Transfer-Encoding: base64" + nl + nl +
+
+		// Body itself
+		body + nl + nl)
+
+	for i := 0; i < len(files); i++ {
+		bytesArray, attachmentType, fileNames, err := attach.GetAttachmentsForEmail(r, email, files)
+		if err == nil {
+			for i := 0; i < len(bytesArray); i++ {
+				str := base64.StdEncoding.EncodeToString(bytesArray[i])
+
+				attachment := []byte(
+					"--" + boundary + nl +
+						"Content-Type: " + attachmentType[i] + nl +
+						"MIME-Version: 1.0" + nl +
+						"Content-Disposition: attachment; filename=\"" + fileNames[i] + "\"" + nl +
+						"Content-Transfer-Encoding: base64" + nl + nl +
+						str + nl + nl,
+				)
+
+				temp = append(temp, attachment...)
+			}
+		}
+	}
+
+	finalBoundry := []byte(
+		"--" + boundary + "--",
+	)
+
+	temp = append(temp, finalBoundry...)
+
+	raw := base64.StdEncoding.EncodeToString(temp)
+	raw = strings.Replace(raw, "/", "_", -1)
+	raw = strings.Replace(raw, "+", "-", -1)
+	raw = strings.Replace(raw, "=", "", -1)
+
+	return raw, nil
+}
 
 func VerifySMTP(servername string, email string, password string) error {
 	host, _, _ := net.SplitHostPort(servername)
